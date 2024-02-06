7c06a008d9dc6a3132b598a2808f91f97276ba5c
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-02-06 09:30:37 +0000

    Finding percentage

--- Python assignments/Nested_lists.py
+++ Python assignments/Nested_lists.py
@@ -1,4 +1,4 @@
-# mylist=[['Rahul', 22.0], ['sumesh', 26.0], ['Amal', 28.0], ['Zhazha', 30.0]]
+#mylist=[['Rahul', 22.0], ['sumesh', 26.0], ['Amal', 28.0], ['Zhazha', 30.0]]
 mylist=[]
 for i in range(int(input())):
     name = input()
@@ -6,21 +6,15 @@ for i in range(int(input())):
     mylist.append([name,score])
     
 mylist.sort(key=lambda x:x[1], reverse=True)
-second_lowest = 0
-if len(mylist) > 2:
-    length = len(mylist)-1
-else:
-    length = len(mylist)
-for i in range (1, length):
-    if mylist[i][1] < mylist[i-1][1]:
-        second_lowest = mylist[i][1]
+length = len(mylist)
+lowest = mylist[length-1][1]
+
+
+mylist=[[name,score] for name,score in mylist if score!=lowest]
+second_lowest = mylist[len(mylist)-1][1]
 
 mylist.sort(key=lambda x:x[0])
 
 for name,score in mylist:
     if(score == second_lowest):
         print(name)
-
-
-# print(second_lowest)
-# print(mylist)
